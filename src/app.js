386b2e1c645d3abe36c83ef7c9d68a5f1e51cf9f
ThiraphatK <thiraphat_khar@cmu.ac.th>
2024-02-03 16:17:50 +0700

    add return json api

--- src/app.js
+++ src/app.js
@@ -3,7 +3,16 @@ const app = express();
 
 app.get('/', (req,res)=>{
     res.send('hello world!');
-})
+});
+
+app.get('/hello', (req,res)=>{
+    const returnObj = {
+        name:"test",
+        age: "20",
+        address: "thai"
+    };
+    res.send(returnObj);
+});
 
 app.listen(3000,()=>{
     console.log('Server is listeming posrt 3000!');
