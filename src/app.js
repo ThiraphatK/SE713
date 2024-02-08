fc6f89ead6619da5bbab5c69b47dbf453ebfffa5
ThiraphatK <thiraphat_khar@cmu.ac.th>
2024-02-08 22:25:54 +0700

    part-4

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
+// part-1
 app.get('/test', (req,res)=>{
     const id = req.query.id;
     const name = req.query.name || 'No name';
@@ -11,17 +13,92 @@ app.get('/test', (req,res)=>{
     res.send(output)
 });
 
+// part-2
 app.get('/test/:id/name/:name', (req,res)=>{
     const id = req.params.id;
     const name = req.params.name;
     res.send(`id: ${id} and name: ${name}`)
 })
 
+// part-3
 app.post('/test', (req,res)=>{
     const obj = req.body;
     const id = obj.id;
     const name = obj.name;
     res.send(`id: ${id} and name: ${name}`)
+})
+
+// part-4
+const events = [
+    {
+        "id": 123,
+        "category": "animal welfare",
+        "title": "Cat Adoption Day",
+        "description": "Find your new feline friend at this event.",
+        "location": "Meow Town",
+        "date": "January 28, 2022",
+        "time": "12:00",
+        "petAllowed": true,
+        "organizer": "Kat Laydee"
+    },
+    {
+        "id": 456,
+        "category": "food",
+        "title": "Community Gardening",
+        "description": "Join us as we tend to the community edible plants.",
+        "location": "Flora City",
+        "date": "March 14, 2022",
+        "time": "10:00",
+        "petAllowed": true,
+        "organizer": "Fern Pollin"
+    },
+    {
+        "id": 789,
+        "category": "sustainability",
+        "title": "Beach Cleanup",
+        "description": "Help pick up trash along the shore.",
+        "location": "Playa Del Carmen",
+        "date": "July 22, 2022",
+        "time": "11:00",
+        "petAllowed": false,
+        "organizer": "Carey Wales"
+    },
+    {
+        "id": 1001,
+        "category": "animal welfare",
+        "title": "Dog Adoption Day",
+        "description": "Find your new canine friend at this event.",
+        "location": "Woof Town",
+        "date": "August 28, 2022",
+        "time": "12:00",
+        "petAllowed": true,
+        "organizer": "Dawg Dahd"
+    },
+    {
+        "id": 1002,
+        "category": "food",
+        "title": "Canned Food Drive",
+        "description": "Bring your canned food to donate to those in need.",
+        "location": "Tin City",
+        "date": "September 14, 2022",
+        "time": "3:00",
+        "petAllowed": true,
+        "organizer": "Kahn Opiner"
+    },
+    {
+        "id": 1003,
+        "category": "sustainability",
+        "title": "Highway Cleanup",
+        "description": "Help pick up trash along the highway.",
+        "location": "Highway 50",
+        "date": "July 22, 2022",
+        "time": "11:00",
+        "petAllowed": false,
+        "organizer": "Brody Kill"
+    }
+]
+app.get('/events', (req,res)=>{
+    res.send(events);
 })
 
 app.listen(3000,()=>{
