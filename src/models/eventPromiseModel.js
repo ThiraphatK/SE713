e316cac4b501ba3380f20b6ed59f4554e14d3779
ThiraphatK <thiraphat_khar@cmu.ac.th>
2024-02-09 22:48:09 +0700

    part-15

--- src/models/eventPromiseModel.js
+++ src/models/eventPromiseModel.js
@@ -11,6 +11,39 @@ function getAllEvents() {
         })
 }
 
+function getEventsById(id) {
+    return pool.query('SELECT * FROM events WHERE id = ?', [id])
+        .then(([rows]) => {
+            return rows[0];
+        }).catch((err) => {
+            console.error('Error getting event:', err);
+            throw err;
+        });
+};
+
+function getEventsByName(name) {
+    return pool.query('SELECT * FROM events WHERE title = ?', [name])
+        .then(([rows]) => {
+            return rows[0];
+        }).catch((err) => {
+            console.error('Error getting event:', err);
+            throw err;
+        });
+};
+
+function getEventsByPatialName(name) {
+    return pool.query('SELECT * FROM events WHERE title LIKE ?', [`%${name}%`])
+        .then(([rows]) => {
+            return rows[0];
+        }).catch((err) => {
+            console.error('Error getting event:', err);
+            throw err;
+        });
+};
+
 module.exports = {
-    getAllEvents
+    getAllEvents,
+    getEventsById,
+    getEventsByName,
+    getEventsByPatialName
 }
--- src/routes/eventDbPromiseRoutes.js
+++ src/routes/eventDbPromiseRoutes.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const eventModel = require('../models/eventPromiseModel');
+const eventModels = require('../models/eventPromiseModel');
 
 router.get('/', (req,res) => {
-    eventModel.getAllEvents()
-        .then((events) => {
-            res.send(events)
-        }).catch((err) => {
-            res.status(500).send('Error getting events')
-        });
+    const name = req.query.name;
+    if (name) {
+        eventModels.getEventsByPatialName(name)
+            .then((event) => {
+                if (!event) {
+                    res.status(400).send('The event with the given name not found');
+                } else {
+                    res.send(event);
+                }
+            }).catch((err) => {
+                res.status(500).send('Error getting event');
+            });
+    } else {
+        eventModels.getAllEvents()
+            .then((event) => {
+                res.send(event);
+            }).catch((err) => {
+                res.status(500).send('Error getting event')
+            });
+    }
 });
 
 module.exports = router;
